4380ea127b75171cca64a50818623cf9f653e9a0
dkadams2 <derrik_adams3@hotmail.com>
2018-03-19 17:35:03 -0600

    Removed unnecessary portion of if statements

--- Hmwk_9.m
+++ Hmwk_9.m
@@ -16,17 +16,11 @@ z = -.75:.0001:.75; %m
 P = 60000; %N
 
 for i=1:length(z)
-    az = a+z(i);
     %If statements are for ABz and CBz, since ABz(-z)=ABz(z) and
     %CBz(-z)=CB(z)
-    if z(i)<0 && az<0
-         y(i) = (P*b/(2*k))*((exp(-b*-z(i))*(sin(b*-z(i))+cos(b*-z(i))))+(2*Dba*exp(-b*(az))*cos(b*(az)))+(Cba*exp(-b*(-az))*((cos(b*(-az)))-sin(b*(-az)))));
-         M(i) = (P/(4*b))*(exp(-b*-z(i))*(cos(b*-z(i))-sin(b*-z(i)))-2*Dba*exp(-b*(a+z(i)))*sin(b*(a+z(i)))-Cba*exp(-b*(-az))*((sin(b*(-az)))+cos(b*(-az))));
-    
-    elseif z(i)<0 && az>=0
+    if z(i)<0
          y(i) = (P*b/(2*k))*((exp(-b*-z(i))*(sin(b*-z(i))+cos(b*-z(i))))+(2*Dba*exp(-b*(a+z(i)))*cos(b*(a+z(i))))+(Cba*exp(-b*(a+z(i)))*((cos(b*(a+z(i))))-sin(b*(a+z(i))))));
-         M(i) = (P/(4*b))*(exp(-b*-z(i))*(cos(b*-z(i))-sin(b*-z(i)))-2*Dba*exp(-b*(a+z(i)))*sin(b*(a+z(i)))-Cba*exp(-b*(a+-z(i)))*((sin(b*(a+-z(i))))+cos(b*(a+-z(i)))));
-         
+         M(i) = (P/(4*b))*(exp(-b*-z(i))*(cos(b*-z(i))-sin(b*-z(i)))-2*Dba*exp(-b*(a+z(i)))*sin(b*(a+z(i)))-Cba*exp(-b*(a+z(i)))*((sin(b*(a+z(i))))+cos(b*(a+z(i)))));
     else
          y(i) = (P*b/(2*k))*((exp(-b*z(i))*(sin(b*z(i))+cos(b*z(i))))+(2*Dba*exp(-b*(a+z(i)))*cos(b*(a+z(i))))+(Cba*exp(-b*(a+z(i)))*((cos(b*(a+z(i))))-sin(b*(a+z(i))))));
          M(i) = (P/(4*b))*(exp(-b*z(i))*(cos(b*z(i))-sin(b*z(i)))-2*Dba*exp(-b*(a+z(i)))*sin(b*(a+z(i)))-Cba*exp(-b*(a+z(i)))*((sin(b*(a+z(i))))+cos(b*(a+z(i)))));
@@ -34,11 +28,11 @@ for i=1:length(z)
 end
 
 figure(1)
-plot(z,y)
+plot(z,y);
 ymax = max(y);
 zmaxy = z(find(y==ymax));
 figure(2)
-plot(z,M)
+plot(z,M);
 Mmax = max(M);
 zmaxm = z(find(M==Mmax));
 
